9246a36cbfbd53049f6e1a8f9e055d6efeec1083
Hadi Rickit <rickithadi@gmail.com>
2024-09-24 14:07:44 +0800

    no formdata

--- src/app.js
+++ src/app.js
@@ -1,27 +1,51 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const { exec } = require("child_process");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+const api = require("./api");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+app.post("/image-upload", (req, res) => {
+  console.log("uploading body", req.body);
+
+  exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return error.message;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return stderr;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
   });
 });
 
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
